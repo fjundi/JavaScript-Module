876291268a6e6c0c5c89418eb0d14fa27166899d
fjundi <30470117+fjundi@users.noreply.github.com>
2017-08-31 22:01:22 +0200

    Add files via upload
    Week 1 JavaScript Homework

---
+++ week1hm.js
@@ -0,0 +1,64 @@
+//1
+
+
+let x = 21;
+
+console.log('x is equal to ' + x);
+
+
+//2
+
+
+let a = 7.25;
+
+Math.round(a);
+
+console.log(Math.round(a));
+
+
+//3 + 4 
+
+
+let colors = ['red', 'green', 'blue'];
+
+console.log('The array is ' + colors.length + ' strings long.');
+
+
+//5
+
+
+var x = 9;
+var y = 'Hello';
+
+if (typeof x == typeof y) {
+    console.log('SAME TYPE');
+}
+else {console.log('NOT SAME TYPE')
+}
+
+
+//6
+
+
+let x = 7;
+
+console.log(x = x % 3); //x = 1
+
+
+//7
+
+
+let types = ['One type', 'Two types', 'THREE TYPES!!', 'AHA AHA AHA AHA', 4, 5]
+
+console.log(types);
+
+
+/**/
+var x = 6/0;
+var y = 10/0;
+if ( x == y){
+console.log('Equal');
+}
+else{
+console.log('Not Equal');
+}
